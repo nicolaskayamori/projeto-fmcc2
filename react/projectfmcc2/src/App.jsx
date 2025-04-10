f0e5b3ce838942723982cfb220a541bb22d3fbed
caio simplicio <caio1simplicio@gmail.com>
2025-04-10 19:18:34 -0300

    instalando react e passando o html pro jsx

---
+++ react/projectfmcc2/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Sistema do Teorema Chinês do Resto</h1>
+    <div>
+      <input type="text" placeholder="3x" inputMode="numeric" />
+      <p>≡</p>
+      <input type="text" placeholder="5" inputMode="numeric" />
+      <p>(mod</p>
+      <input type="text" placeholder="10" inputMode="numeric" />
+      <p>)</p>
+    </div>
+  </>
+  )
+}
+
+export default App
