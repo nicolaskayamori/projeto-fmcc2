8b0f5f638cb97b3bc8b42c355618a77dc4474daf
caio simplicio <caio1simplicio@gmail.com>
2025-04-10 22:06:47 -0300

    ajustando css

--- react/projectfmcc2/src/App.jsx
+++ react/projectfmcc2/src/App.jsx
@@ -4,18 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [ladoEsquerdo, setLadoEsquerdo] = useState(0);
+  const [ladoDireito, setLadoDireito] = useState(0);
+  const [modulo, setModulo] = useState(0);
 
   return (
     <>
-    <h1>Sistema do Teorema Chinês do Resto</h1>
+    <h1 className='titulo'>Sistema do Teorema Chinês do Resto</h1>
     <div className='inputs'>
-      <input type="text" placeholder="3x" inputMode="numeric" />
-      <p>≡</p>
-      <input type="text" placeholder="5" inputMode="numeric" />
-      <p>(mod</p>
-      <input type="text" placeholder="10" inputMode="numeric" />
-      <p>)</p>
+      <form className='inputs'>
+        <input onBlur={(e) => setLadoEsquerdo(e.target.value)} type="text" placeholder="3x" inputMode="numeric" required />
+        <p>≡</p>
+        <input onBlur={(e) => setLadoDireito(e.target.value)} type="text" placeholder="5" inputMode="numeric" required />
+        <p>(mod</p>
+        <input onBlur={(e) => setModulo(e.target.value)} type="text" placeholder="10" inputMode="numeric" />
+        <p>)</p>
+        <button type='submit' className='buttonSubmit'>
+          Enviar
+        </button>
+      </form>
     </div>
   </>
   )
