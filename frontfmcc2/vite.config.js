84e797827cc1d291e20ca0e20796b321bc9a2cf3
Nicolas K Huschak <nicolaskayamori@gmail.com>
2025-04-21 00:07:28 -0300

    Corrige erro no deploy

--- frontfmcc2/vite.config.js
+++ frontfmcc2/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/projeto-fmcc2/', // <- ESSENCIAL para funcionar no GitHub Pages
   server: {
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 5173,      // Explicitly set the port (default is 5173)
-    strictPort: false, // Exit if port is already in use
-    open: true,      // Automatically open the app in the browser
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+    open: true,
   },
-});
+})
